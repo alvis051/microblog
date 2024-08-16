1176da5329ee918eb0365afcb45dd6896e4eeb1e
alvis051 <chenhangoh051@gmail.com>
2024-08-16 14:38:32 +0800

    v3.0 Web Forms

--- app/routes.py
+++ app/routes.py
@@ -25,6 +25,6 @@ def login():
         flash('Login requested for user {}. remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
